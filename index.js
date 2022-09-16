0a56274cc50204be4f1c34735ca8e5cc67bc3ff5
Tushar Chaudhari <tushar.c8942@gmail.com>
2022-09-16 12:23:36 +0530

    User Auth Done

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mongoose=require("mongoose");
 const dotenv=require("dotenv");
 const cors=require("cors");
 
-// const authRoute=require("./routes/auth");
-// const usersRoute=require("./routes/user");
+const authRoute=require("./routes/auth");
+const usersRoute=require("./routes/user");
 // const productsRoute=require("./routes/product");
 // const cartRoute=require("./routes/cart");
 // const orderRoute=require("./routes/order");
@@ -21,14 +21,14 @@ mongoose
 //Routes used 
 app.use(cors());
 app.use(express.json());
-// app.use("/shopify/auth",authRoute);
-// app.use("/shopify/users",usersRoute);
+app.use("/crypto/auth",authRoute);
+app.use("/crypto/users",usersRoute);
 // app.use("/shopify/products",productsRoute);
 // app.use("/shopify/cart",cartRoute);
 // app.use("/shopify/orders",orderRoute);
 // app.use("/shopify/checkout",stripeRoute);
 
-// process.env.PORT || 
+// Server Started....
 app.listen(process.env.PORT || 3000,()=>{           //Server Starting process...npm start
     console.log("Backend Server is Running...")
 });
